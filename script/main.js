678211b9ebe70d4610444867037e0f67d7da7928
711GHOST <shwetanshusood9102001@gmail.com>
2024-02-12 23:14:11 +0530

    Valentine's Commit

--- script/main.js
+++ script/main.js
@@ -269,10 +269,10 @@ const animationTimeline = () => {
   // tl.timeScale(2);
 
   // Restart Animation on click
-  const replyBtn = document.getElementById("replay");
-  replyBtn.addEventListener("click", () => {
-    tl.restart();
-  });
+  // const replyBtn = document.getElementById("replay");
+  // replyBtn.addEventListener("click", () => {
+  //   tl.restart();
+  // });
 };
 
 // Import the data to customize and insert them into page
